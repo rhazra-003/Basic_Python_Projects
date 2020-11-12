0228d6e5c099bf09b9a2ce62f77e7be8c10ea32d
Ridam Hazra <rhazra0602@gmail.com>
2020-11-12 20:49:22 +0530

    Add files via upload

---
+++ Random Password Generator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    print("Random string of length", length, "is:", result_str)
+
+get_random_string(9)
+get_random_string(7)
+get_random_string(5)
