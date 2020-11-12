653e4adc0ec769cfabe438a438bd74d642198f0b
Ridam Hazra <rhazra0602@gmail.com>
2020-11-12 17:39:35 +0530

    Add files via upload

---
+++ Mad Libs Generator.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+footballer = input("Enter a footballer: ")
+
+print("Roses are", color)
+print(pluralNoun + " are blue")
+print("I love", footballer)
